c50981c048be0715c695b1fb16f5b74b62d13c9d
Rati Elizbarashvili <elizbarashvilirati15@Gmail.com>
2024-11-05 03:42:44 +0400

    all done✅ chemi nervebi✅

--- script.js
+++ script.js
@@ -4,3 +4,65 @@
 // ჩემი დამატებული ინფორმაცია , თითოეულ li აითემს უნდა ქონდეს საკუთარი წაშლის ღილაკი და დაჭერის შემთვევაში
 // უნდა იშლებოდეს ის ინფორმაცია რომელსაც დავაჭერ.
 
+let button = document.getElementById("mainbtn")
+
+let ultag = document.getElementById("ultag")
+
+
+button.addEventListener("click", () => {
+
+    let info = prompt("შეიყვანეთ ინფორმაცია");
+    
+
+    if(info == false){
+        alert("error");
+    }else{
+        let container = document.createElement("div");
+        let litag = document.createElement("li");
+        litag.innerText = info;
+        ultag.appendChild(container);
+        container.appendChild(litag);
+        if(litag){
+            let clearbut = document.createElement("button");
+            clearbut.innerText = "clear";
+            container.appendChild(clearbut);
+            clearbut.addEventListener("click" ,  () => {
+            container.remove(litag);
+        })
+    }
+}})
+
+// let button = document.getElementById("mainbtn")
+
+// let ultag = document.getElementById("ultag")
+
+// button.addEventListener("click", () => {
+//     let info = prompt("შეიყვანეთ ინფორმაცია")
+//         if(info == false){
+//             alert("error")
+//         }else{
+//             let container = document.createElement("div");
+//         let litag = document.createElement("li");
+//         litag.innerText = info;
+//         ultag.appendChild(container);
+//         container.appendChild(litag);
+//         if(litag){
+//             let clearbut = document.createElement("button")
+//         clearbut.innerText = "clear"
+//         container.appendChild(clearbut);
+//         clearbut.addEventListener("click" ,  () => {
+//         container.remove(litag)
+//         })
+//         }
+//         }
+//         // if(litag){
+//         //     let clearbut = document.createElement("button")
+//         // clearbut.innerText = "clear"
+//         // container.appendChild(clearbut);
+//         // clearbut.addEventListener("click" ,  () => {
+//         // ultag.remove(litag)
+//         // })
+//         // }
+//     }
+// )
+
